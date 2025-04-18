0dda6cbd3661c9417c43a582b913e7ebf42654c8
baggiiiie <yc@yingchaoooo.com>
2025-04-18 12:11:24 +0800

    feat: add textnode to htmlnode converter

---
+++ src/test_utils.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+from utils import text_node_to_html_node
+
+
+class TestUtils(unittest.TestCase):
+    def test_textnode_no_tag(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textnode_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textnode_invalid_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+
+if __name__ == "__main__":
+    unittest.main()
