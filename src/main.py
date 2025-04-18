027765444fffc232154b659a9dd7a5e3f10d7b6b
baggiiiie <yc@yingchaoooo.com>
2025-04-18 10:36:05 +0800

    feat: add TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    my_node = TextNode("text", TextType.LINK, "www.google.com")
+    print(my_node)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    NORMAL = "normal_text"
+    BOLD = "bold_text"
+    ITALIC = "italic_text"
+    CODE = "code_text"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        if other is None:
+            return False
+        if other.text != self.text:
+            return False
+        if other.text_type != self.text_type:
+            return False
+        if other.url != self.url:
+            return False
+        return True
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
