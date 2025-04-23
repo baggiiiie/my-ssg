0a24ad48aabf19fc865701d0fa968c5ec40ec45a
baggiiiie <yc@yingchaoooo.com>
2025-04-24 00:48:19 +0800

    UT: add get block type test

--- tests/test_get_block_type.py
+++ tests/test_get_block_type.py
@@ -42,15 +42,29 @@ class TestBlockToBlockType(unittest.TestCase):
             BlockType.QUOTE,
         )
 
-    def test_get_block_type_quote_2(self):
-        block = "> test\n> test2"
-        self.assertEqual(
-            get_block_type(block),
-            BlockType.QUOTE,
-        )
-
     def test_get_block_type_not_quote(self):
         block = ">test"
+        self.assertEqual(
+            get_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_get_block_type_unordered_list(self):
+        block = "- test\n- test2"
+        self.assertEqual(
+            get_block_type(block),
+            BlockType.UNORDERED_LIST,
+        )
+
+    def test_get_block_type_ordered_list(self):
+        block = "1. test\n2. test2"
+        self.assertEqual(
+            get_block_type(block),
+            BlockType.ORDERED_LIST,
+        )
+
+    def test_get_block_type_ordered_list_failed(self):
+        block = "1. test\n3. test2"
         self.assertEqual(
             get_block_type(block),
             BlockType.PARAGRAPH,
