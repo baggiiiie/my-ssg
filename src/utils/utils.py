ed7ea5cd9ccd2eae5e2803ff5145345f5e085883
baggiiiie <yc@yingchaoooo.com>
2025-04-24 15:23:24 +0800

    chore: code clean up

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,11 @@
-from typing import ParamSpec
-from src.htmlblock import BlockChildrenType, BlockType, BLOCK_CHILDREN_MAP
+from src.htmlblock import BLOCK_CHILDREN_MAP
 from src.utils.block_checker import get_block_type
 from src.nodes.htmlnode import HTMLNode, LeafNode, ParentNode
 from src.utils.node_utils import str_to_textnodes, textnode_to_leafnode
-from src.utils.str_utils import format_block, md_to_blocks, trailing_spaces_to_new_line
+from src.utils.str_utils import format_block, md_to_blocks
 
 
 def md_to_htmlnode(md: str) -> HTMLNode:
-    # split by `\n\n`
     blocks = md_to_blocks(md)
     block_nodes = []
     for block in blocks:
@@ -33,25 +31,3 @@ def md_to_htmlnode(md: str) -> HTMLNode:
 
     leaf_node = HTMLNode(tag="div", children=block_nodes)
     return leaf_node
-
-    # if BLOCK_CHILDREN_MAP[block_type]:
-    #     print(block_type)
-    #     print(BLOCK_CHILDREN_MAP[block_type])
-    #     children_nodes = [
-    #         ParentNode(
-    #             tag=BLOCK_CHILDREN_MAP[block_type].value,
-    #             children=children_nodes,
-    #         )
-    #     ]
-    #     parent_node = ParentNode(
-    #         tag=block_type.value,
-    #         children=children_nodes,
-    #     )
-    #     parent_nodes.append(parent_node)
-    #
-    # if block_type == BlockType.CODE:
-    #     child_node = LeafNode(tag="code", value=block)
-    #     block_nodes.append(ParentNode(tag="pre", children=[child_node]))
-    #     continue
-    # elif block_type == BlockType.UNORDERED_LIST:
-    #     children_nodes = parent_nodes
