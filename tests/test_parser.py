5a9b71df05418efc5d5c22f21c3000e8ae3bc16a
baggiiiie <yc@yingchaoooo.com>
2025-05-01 22:40:54 +0800

    feat: add tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,64 @@
 import unittest
-from parser import MarkdownParser
+from src.parser import MarkdownParser
 
 
-class TestParser(unittest.TestCase):
-    def test_parser(self):
+class TestInlineParser(unittest.TestCase):
+    def test_inline_parser_normal_text(self):
         md = "this is a test"
-        parser = MarkdownParser()
-        md = parser.inline_parser(md)
-        self.assertEqual(md, "this is a test")
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is a test"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_code(self):
+        md = "this is `code block`"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <code>code block</code>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_italic(self):
+        md = "this is *italic*"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <i>italic</i>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_italic_2(self):
+        md = "this is _italic_"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <i>italic</i>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_bold(self):
+        md = "this is **bold**"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <b>bold</b>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_bold_2(self):
+        md = "this is __bold__"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <b>bold</b>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_multiple(self):
+        md = "this is __bold__ and *italic*"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <b>bold</b> and <i>italic</i>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_multiple_2(self):
+        md = "this is __bold__ and `code`"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <b>bold</b> and <code>code</code>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_nested(self):
+        md = "this is __bolded *italic*__"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <b>bolded <i>italic</i></b>"
+        self.assertEqual(result, expected)
+
+    def test_inline_parser_normal_nested_2(self):
+        md = "this is `__non-bolded__ code`"
+        result = MarkdownParser().inline_parser(md)
+        expected = "this is <code>__non-bolded__ code</code>"
+        self.assertEqual(result, expected)
