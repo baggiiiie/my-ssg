a925b5cb0cd3b81ef3834975cd8deb8cd9e0e897
baggiiiie <yc@yingchaoooo.com>
2025-05-01 15:15:26 +0800

    feat: update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from src.utils.utils import md_to_htmlnode
+from src.utils.file_utils import copy_dir
+from src.utils.utils import generate_page
+
 
 if __name__ == "__main__":
-    md3 = """
-        this is a [link](https://test) and an ![img](img_link)
-        """
-
-    node = md_to_htmlnode(md3)
-    print(rf"{node.to_html()}")
-    print("-" * 50)
-    # node = md_to_htmlnode(md2)
-    # print(node.to_html())
-    # print("-" * 50)
-    # node = md_to_htmlnode(md3)
-    # print(node.to_html())
+    copy_dir()
+    generate_page()
