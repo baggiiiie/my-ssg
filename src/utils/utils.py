4f50cfe49632600eb2cb9911abc92c28d4167f14
baggiiiie <yc@yingchaoooo.com>
2025-05-08 21:17:56 +0800

    refactor: merge utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,6 +6,41 @@ from utils.node_utils import str_to_textnodes, textnode_to_leafnode
 from utils.str_utils import format_block, md_to_blocks, extract_title
 
 
+import re
+from type import LineType, InlineTextType
+
+LINE_REGEX = {
+    LineType.HEADING: re.compile(r"^(#{1,6})\s+(.+)$"),
+    LineType.QUOTE: re.compile(r"^>\s*(.*)$"),
+    LineType.CODE_START: re.compile(r"^```(\w*)$"),
+    LineType.CODE_END: re.compile(r"^```$"),
+    LineType.UNORDERED_LIST_ITEM: re.compile(r"^(\s*)[-*+]\s+(.+)$"),
+    LineType.ORDERED_LIST_ITEM: re.compile(r"^(\s*)\d+\.\s+(.+)$"),
+    LineType.HORIZONTAL_RULE: re.compile(r"^([\*\-_])\1{2,}$"),
+}
+
+INLINE_REGEX = {
+    InlineTextType.INLINE_CODE: (re.compile(r"`([^`]+)`"), "<code>{}</code>"),
+    InlineTextType.IMAGE: (
+        # TOOD: link replacement is currently broken
+        re.compile(r"!\[([^\]]*)\]\(([^)]+)\)"),
+        r'<img src="\2" alt="\1">',
+    ),
+    InlineTextType.LINK: (
+        re.compile(r"\[([^\]]+)\]\(([^)]+)\)"),
+        r'<a href="\2">\1</a>',
+    ),
+    InlineTextType.BOLD: (
+        re.compile(r"\*\*([^*]+)\*\*|__([^_]+)__"),
+        lambda m: f"<b>{m.group(1) or m.group(2)}</b>",
+    ),
+    InlineTextType.ITALIC: (
+        re.compile(r"\*([^*]+)\*|_([^_]+)_"),
+        lambda m: f"<i>{m.group(1) or m.group(2)}</i>",
+    ),
+}
+
+
 def md_to_htmlnode(md: str) -> HTMLNode:
     blocks = md_to_blocks(md)
     block_nodes = []
